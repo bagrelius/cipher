eb547522827a4bb75d7266c7cce41e0e4cea2930
bagrelius <bagrelius@gmail.com>
2023-11-30 20:05:24 -0700

    Started on random scramble

--- cypther.cpp
+++ cypther.cpp
@@ -1,8 +1,39 @@
 #include <iostream> 
 
+
+
+void scramble(std::string& inputString) {
+
+    //Just using the random library (not fully unpredictable)
+    char letters[] = "abcdefghijklmnopqrstuvwxyz";
+    char x; 
+    int len = inputString.length();
+
+    for(int i = 0; i < len; i++) {
+        
+        srand(time(0));
+
+        x = letters[rand() % 26];
+        
+        inputString[i] = x;
+    } 
+
+    return;
+}
+
 int main() {
 
-    std::cout << "Hello World" << std::endl;
+    std::string instring = "BOGUS";
+
+    scramble(instring);
+
+    std::cout << instring << std::endl;
 
     return 0;
 }
+
+
+
+
+
+
